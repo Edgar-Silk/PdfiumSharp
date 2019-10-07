0dbc9632def02701b435fdfa9d364d2ad7a8c736
edgar <edgar@SAWAYAMA>
2019-10-07 15:20:19 +0900

    r5uw

--- Build_PdfiumSharp.ps1
+++ Build_PdfiumSharp.ps1
@@ -12,16 +12,7 @@ $BuildDir = (Get-Location).path
 
 # コンフィグ
 Write-Host "Architecture: " $Arch
-Write-Host "Wrapper branch: " $Pdfium_Branch
 Write-Host "Directory to Build: " $BuildDir
-
-# 環境変数を設定する
-$env:Path = "$BuildDir/depot_tools;$env:Path"
-$env:DEPOT_TOOLS_WIN_TOOLCHAIN = "0"
-$env:DEPOT_TOOLS_UPDATE = "0"
-
-# ディレクトリーを設定する
-$WrapperDir = $BuildDir+'/Wrapper'
 
 # ビルドテンポラリーパスを設定する
 if ([System.IO.Directory]::Exists($WrapperDir)) {
@@ -60,3 +51,60 @@ $Project_Name = 'PdfiumSharp'
 
 buildVS -path "C:/projects/pdfiumsharp/PdfiumSharp.sln"
 
+# DLLが存在するかチェックしWrapper/Libへコピーする
+Write-Host "Checking for PDFium.DLL library..."
+Set-Location $BuildDir'/pdfium'
+
+if ($Arch -eq 'x64') {
+    $OUT_DLL_DIR = $BuildDir + '/Lib/x64'
+}
+elseif ($Arch -eq 'x86') {
+    $OUT_DLL_DIR = $BuildDir + '/Lib/x86'
+}
+else {
+    Write-Host "Arch not defined or invalid..."
+    Exit
+}
+
+# solutionをコピーする
+Write-Host "Copy pdfium DLL to Wrapper solution project"
+
+$Lib_Dir = $BuildDir+"/"+$Project_Name+"/lib/"+$Arch
+
+if ([System.IO.Directory]::Exists( $Lib_Dir )) {
+    Set-Location $Lib_Dir
+}
+else {
+    New-Item -Path $Lib_Dir -ItemType Directory
+    Set-Location $Lib_Dir
+}
+
+if (Test-Path -Path $OUT_DLL_DIR'/pdfium.dll') {
+    Copy-Item $OUT_DLL_DIR'/pdfium.dll' -Destination $Lib_Dir
+}
+
+# NuGetパッケージを作成する
+Write-Host "Make NuGet Package..."
+
+Set-Location $BuildDir"/"$Project_Name
+nuget pack SilkWrapperNET.csproj -properties "Configuration=Release;Platform=$Arch"
+
+# ビルドのテンポラリーパスを設定する
+$OUT_NUGET_DIR = $BuildDir+'/NuGet/'+$Arch
+
+# 最終のNuGetパッケージを作成する
+if ([System.IO.Directory]::Exists($OUT_NUGET_DIR)) {
+    Set-Location $OUT_NUGET_DIR
+}
+else {
+    New-Item -Path $OUT_NUGET_DIR -ItemType Directory
+    Set-Location $OUT_NUGET_DIR
+}
+
+Write-Host 'Copy NuGet files output to: ' $OUT_NUGET_DIR
+
+Copy-Item -Path "$BuildDir/$Project_Name/*.*.*.*.nupkg" -Destination $OUT_NUGET_DIR
+
+Set-Location $BuildDir
+
+ls
