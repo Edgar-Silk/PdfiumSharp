311617beb6f919a35a579402d1b13fa7af255918
edgar <edgar@SAWAYAMA>
2019-10-15 14:07:31 +0900

    eafa

---
+++ Test_Unit.ps1
@@ -0,0 +1,49 @@
+﻿# PDFium.DLLをビルドするためのスクリプト -- depot_toolsをインクルードする
+# パラメーター
+param (
+    # オプション: x86 | x64
+    [string]$Arch = 'x64',
+    # 予約パラメーター
+    [string]$Wrapper_Branch = ' '
+)
+
+# Project Name
+$Project_Name = 'PdfiumSharp.Test'
+
+# ビルドディレクトリー
+$BuildDir = (Get-Location).path
+
+# コンフィグ
+Write-Host "Architecture: " $Arch
+Write-Host "Directory to Build: " $BuildDir
+
+# Visual Studio MSI-Builder - コンパイラーを設定する
+Write-Host "Locate VS 2017 MSBuilder.exe"
+function buildVS {
+    param (
+        [parameter(Mandatory=$true)]
+        [String] $path,
+
+        [parameter(Mandatory=$false)]
+        [bool] $clean = $true
+    )
+    process {
+        $msBuildExe = Resolve-Path "${env:ProgramFiles(x86)}/Microsoft Visual Studio/2017/*/MSBuild/*/bin/msbuild.exe" -ErrorAction SilentlyContinue
+
+        if ($clean) {
+            Write-Host "Cleaning $($path)" -foregroundcolor green
+            & "$($msBuildExe)" "$($path)" /t:Clean /m /p:Configuration=Release,Platform=$Arch /v:n
+        }
+
+        Write-Host "Building $($path)" -foregroundcolor green
+        & "$($msBuildExe)" "$($path)" /t:Build /m /p:Configuration=Release,Platform=$Arch /v:n
+    }
+}
+
+buildVS -path "$BuildDir/$Project_Name/$Project_Name.csproj"
+
+
+
+Set-Location $BuildDir
+
+ls
