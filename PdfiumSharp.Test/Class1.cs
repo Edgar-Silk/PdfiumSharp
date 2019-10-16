d7cb26919220810923befb3d0c4cdd2339be2a59
edgar <edgar@SAWAYAMA>
2019-10-16 11:13:26 +0900

    ydiydy

--- PdfiumSharp.Test/Class1.cs
+++ PdfiumSharp.Test/Class1.cs
@@ -15,20 +15,36 @@ namespace PdfiumSharp.Test
     public void LoaderTest() 
     {
       PDF P = new PDF();
+
+      string dir = AppDomain.CurrentDomain.BaseDirectory;
       string File = "test.pdf";
 
       Console.WriteLine("\nOpen PDF file: " + File);
 
+      string all = dir + File;
+
+      Console.WriteLine(all);
+
       //IntPtr doc = Native.FPDF_LoadDocument(File, null);
-      PDF.Load(File);
+      PDF.Load(all);
 
       Console.WriteLine("\nNumber of Pages:" + PDF.PageCount().ToString());
 
       int expectedResult = PDF.PageCount();
 
-      Assert.That(expectedResult, Is.EqualTo(31));
+      Assert.That(expectedResult, Is.EqualTo(311));
 
     }
 
+   [Test]
+    public void AddTest() 
+   {
+      string path = AppDomain.CurrentDomain.BaseDirectory;
+      string dir = NUnit.Framework.TestContext.CurrentContext.TestDirectory;
+      Console.WriteLine(dir);
+      Console.WriteLine(path);
+      Assert.That(5, Is.EqualTo(5));
+   }
+    
   }
 }
